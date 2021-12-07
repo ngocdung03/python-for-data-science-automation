28584adf03fe666056bee0a3429ece604ac08422
ngocdung <dungdidom@gmail.com>
2021-12-07 14:54:13 +0900

    Function

--- DS4B_101P_Course/02_sql_database/02_sqlalchemy.py
+++ DS4B_101P_Course/02_sql_database/02_sqlalchemy.py
@@ -15,22 +15,46 @@ engine = sql.create_engine("sqlite:///00_database/bike_orders_database.sqlite") 
 conn = engine.connect()  #connect your sql alchemy engine object to your database
 
 # Read Excel Files
-
+bikes_df = pd.read_excel("./00_data_raw/bikes.xlsx")
+bikeshops_df = pd.read_excel("./00_data_raw/bikeshops.xlsx")
+orderlines_df = pd.read_excel("./00_data_raw/orderlines.xlsx")
 
 # Create Tables
+bikes_df.to_sql("bikes", con=conn)
+pd.read_sql("SELECT * FROM bikes", con = conn)
 
+bikeshops_df.to_sql("bikeshops", con=conn)
+pd.read_sql("SELECT * FROM bikeshops", con = conn)
+
+orderlines_df \
+    .iloc[:,1:] \
+        .to_sql("orderlines", con=conn, if_exists="replace")  #drop Unnamed column
+pd.read_sql("SELECT * FROM orderlines", con = conn)
 
 # Close Connection
+conn.close()
 
 # RECONNECTING TO THE DATABASE 
 
 # Connecting is the same as creating
-
+engine = sql.create_engine("sqlite://00_database/bikes_oerders_database.sqlite")
+conn = engine.connect()
 
 # GETTING DATA FROM THE DATABASE
 
 # Get the table names
+engine.table_names()  #deprecated
 
+inspector = sql.inspect(conn)
+
+inspector.get_schema_names()  #Database schema: this is the architechture of your database. It has a name. In our case it is "main"
+
+inspector.get_table_names()  #Return the table names from an inspector object that is connected to your database.
+#=inspector.get_table_names('main')
 
 # Read the data
+table = inspector.get_table_names()
+pd.read_sql(f"SELECT * FROM {table[0]}", con=conn)  #Text manipulation with f-string: format text using glue-style syntax where we can add variables to the text
 
+# Close connection
+conn.close()
