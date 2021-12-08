cd9e4440a8966258f4bb528435e7883d942789f8
ngocdung <dungdidom@gmail.com>
2021-12-08 23:27:57 +0900

    Joining

--- DS4B_101P_Course/03_pandas_core/01_data_structures_for_analysis.py
+++ DS4B_101P_Course/03_pandas_core/01_data_structures_for_analysis.py
@@ -2,62 +2,97 @@
 # Module 3 (Pandas Core): Data Structures ----
 
 # IMPORTS ----
+import pandas as pd
+import numpy as np
 
+from my_pandas_extensions.database import collect_data
+df = collect_data()
+df
 
 
 # 1.0 HOW PYTHON WORKS - OBJECTS
 
 # Objects
-
+type(df) 
 
 # Objects have classes
-
+type(df).mro()  #get the object's inheritance structure in the order that methods are searched for.
 
 # Objects have attributes
+df.shape
 
+df.columns
 
 
 # Objects have methods
+df.query("model == 'Jekyll Carbon 2'")
 
 
 
-
-# 2.0 KEY DATA STRUCTURES
+# 2.0 KEY DATA STRUCTURES FOR ANALYSIS
 
 # - PANDAS DATA FRAME
-
-
+# has column and index attributes
+type(df)
 
 # - PANDAS SERIES
+type(df['order_date'])
 
+df['order_date'].dt.year
 
-
+df.dt #error
 
 # - NUMPY
+type(df['order_date'].values).mro()
 
-
-
+# Data Types
+type(df['price'].values).mro()
+df['price'].values.dtype
 
 
 
 # 3.0 DATA STRUCTURES - PYTHON
 
 # Dictionaries
-
-
+d = {'a': 1}
+type(d)
+d.keys()
+d.values()
+d['a']
 
 # Lists
-
-
+l = [1, "A", [2, "B"]]
+l[0]
+l[2]
+list(d.values())[0]  #list conversion (casting)
 
 # Tuples
-
+type(df.shape).mro()
+t = (10, 20)
+t[0] = 20 #error
 
 # Base Data Types
-
-
+type(1.5).mro()
+type(1).mro()
+df.total_price.values
+type(df['model'].values[0])
 
 
 # Casting
+model = "Jekyll Carbon 2"
+price = 6070
+f"The first model is : {model}"
+f"The price of the first model: {price}"  #f-strings automatically data-type converses.
+str(price) + " Some Text" 
+str(1.0)
+int("50%".replace("%", ""))
 
+# Go from low-level to high-level through casting
+type(range(1, 50)).mro()  #range() is a generator, a memory saving strategy that only creates the range when you need it.
+r = list(range(1,50))
+np.array(r)
+pd.Series(r).to_frame()
+
+# Converrint Column Data types
+df['order_date'].astype('str').str.replace("-", "/")   #S.astype() converts a serires from one dtype to another dtype.
 
--- DS4B_101P_Course/03_pandas_core/02_data_wrangling.py
+++ DS4B_101P_Course/03_pandas_core/02_data_wrangling.py
@@ -2,61 +2,111 @@
 # Week 2 (Data Wrangling): Data Wrangling ----
 
 # IMPORTS
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+from my_pandas_extensions.database import collect_data
 
 
 # DATA
-
+df = collect_data()
+df
 
 
 # 1.0 SELECTING COLUMNS
 
 # Select by name
-
+df[['order_date', 'order_id', 'order_line']]
 
 # Select by position
-
+df.iloc[:, 0:3]
+df.iloc[:,-3:]  #last three
 
 # Select by text matching
-
+df.filter(regex = "(^model)|(^cat)", axis=1)
+df.filter(regex ="(price$)|(date$)", axis=1)
 
 # Rearranging columns
+# - Single approach
+l = df.columns.tolist()
+l.remove('model')
+l
+df[['model', *l]]   #* unpack the list
 
+# - Multiple approach
+l.remove('category_1')
+l.remove('category_2')
+df[['model', 'category_1', 'category_2', *l]]
+
+# - List comprehension
+l = df.columns.tolist()
+l
+cols_to_front = ['model', 'category_1', 'category_2']
+
+l2 = [col for col in l if col not in cols_to_front]
+df[[*cols_to_front, *l2]]
 
 # Select by data types
+df.info()
+df1 = df.select_dtypes(include=object)
+df2 = df.select_dtypes(exclude=object)
+pd.concat([df1,df2], axis=1)  # Note that concat requires intermediate dataframes while list comprehension doesn't.
 
 
 # Dropping Columns (De-selecting)
-
+df.drop(['model', 'category_1', 'category_2'], axis=1)
 
 
 # 2.0 ARRANGING ROWS ----
-
-
-
+df.sort_values('total_price', ascending=False)
+df.sort_values('order_date', ascending=False)
+df['price'].sort_values(ascending=False)
+df.sort_values(['category_2', 'order_date'])
 
 # 3.0 FILTERING  ----
 
 # Simpler Filters
-
+df['order_date'] >= pd.to_datetime("2015-01-01")
+df[df['order_date'] >= pd.to_datetime("2015-01-01")]
+df.model == "Trigger Carbon 1"
+df.model.str.startswith("Trigger")
+df[df.model.str.contains("Carbon")]
 
 # Query
+price_threshold1 = 9000
+price_threshold2 = 1000
+df.query("(price >= @price_threshold1)|(price <= @price_threshold2)")  # we can use this method inside of method chaining.
 
+df.query(f"price >= {price_threshold1}")
 
 # Filtering Items in a List
+df['category_2'].unique()
+df['category_2'].value_counts()
+df[df['category_2'].isin(['Triathalon', 'Over Mountain'])]
 
+# Alternative: series.str.contains(regex='(Trialthalon)|(Over Mountain)')
+
+df[~ df['category_2'].isin(['Triathalon', 'Over Mountain'])]  # The ~ flips the boolean series to form the opposite True/False
 
 # Slicing
-
+df[:5]
+df.head(5)
+df.tail(5)
 
 # Index Slicing
-
+df.iloc[0:5, [1,3,5]] #loc is ised for row and columns selections with names. iloc is for index positions
+df.iloc[0:5,:]
+df.iloc[0:5]
+df.iloc[:, [1,3,5]]
 
 # Unique / Distinct Values
+df[['model', 'category_1', 'category_2', 'frame_material']]\
+    .drop_duplicates()
 
-
+df['model'].unique()
 # Top / Bottom
-
+df.nlargest(n = 20, columns = 'total_price')
 
 # Sampling Rows
 
